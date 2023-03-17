cafbd67109cb31e462ab5df45f3a5a6133642ae4
chuntiande <2358432189@qq.com>
2023-03-17 15:28:52 +0800

    judge that it is dual_mode or single_mode, read the fre of OpenLDI video which is input

--- err_to_correct.c
+++ err_to_correct.c
@@ -19,6 +19,7 @@ static int i2cset_dual_pair(struct i2c_client *client);
 static int set_gpio_input(struct i2c_client *client);
 static int link_detect_8(struct i2c_client *client);
 static int rd_ODLI_frequency(struct i2c_client *client);
+static int read_video_frequency(struct i2c_client *client);
 
 static int i2crdreg_open(struct inode *inode, struct file *file)
 {
@@ -76,7 +77,7 @@ static int i2cread_regs_8(struct i2c_client *client)
 	u8 addr;
 	addr = paddr&0xff;
 
-	printk(KERN_CRIT"This is %s \n", __func__);
+	printk(KERN_CRIT"\n This is %s ", __func__);
 
 	for(i = 0; i < 2; i++)
 	{
@@ -103,6 +104,103 @@ static int i2cread_regs_8(struct i2c_client *client)
 	return 0;
 }
 
+//读寄存器函数:判断OLDI接口是单像素还是双像素模式，再读取OpenLDI的输入视频频率
+static int read_video_frequency(struct i2c_client *client)
+{
+	int ret = 0;
+	int i = 0;
+	int r = 0;
+	//将十进制转换为二进制的间接数
+	int m, k;
+	//存放二进制数的数组
+	u8 bin_arr[] = {0};
+	u8 buf[1];
+	int mediant;
+	u8 pixel_mode = 0;
+	u8 pixel_frequency = 0;
+	u8 video_frequency = 0;
+	//读取LINK状态位的寄存器地址
+	u8 reg[] = {0x4f, 0x5f};
+	//client->addr = 0x0c;
+	u32 paddr = 0x0c;
+	struct i2c_msg msg[2];
+	u8 addr;
+	addr = paddr&0xff;
+
+	printk(KERN_CRIT"\n This is %s", __func__);
+	//判断OLDI接口是单像素还是双像素模式
+	buf[0] = reg[0];
+	msg[0].addr  = addr;
+	msg[0].flags = client->flags;   //flags为0，是写，表示buf是我们要发送的数据
+	msg[0].buf   = buf;             //寄存器地址
+	msg[0].len   = sizeof(buf);     //len为buf的大小，单位是字节
+
+	msg[1].addr  = addr;
+	msg[1].flags = client->flags | IIC_RD;   //flags为1，是读，表示buf是我们要接收的数据
+	msg[1].buf   = buf;          //寄存器的值
+	msg[1].len   = 1;
+	ret = i2c_transfer(client->adapter, msg, 2);
+	if(ret < 0)
+	{
+		printk(KERN_CRIT"read regs from i2c has been failed, ret = %d \r\n", ret);
+		return ret;
+	}
+	pixel_mode = buf[0];
+	printk(KERN_CRIT"default regadd: 0x%02x---pixel_mode is 0x%02x  hex\n binary pixel_mode is ", reg[0], pixel_mode);
+
+	for(i = 0;i < 8; i++)
+	{
+		m = pixel_mode%2;  //取2的余数
+		k = pixel_mode/2;  //取被2整除的结果
+		pixel_mode = k;
+		bin_arr[i] = m;    //将余数存入数组bin_arr数组中
+		printk(KERN_CONT"%d", bin_arr[i]);
+	}
+	/*
+	for(i = 7; i >= 4; i--)
+	{
+		mediant = bin_arr[i];
+		bin_arr[i] = bin_arr[7-i];
+		bin_arr[7-i] = mediant; 
+		printk(KERN_CONT"%d", bin_arr[i]);
+	}
+	*/
+
+	//读取OpenLDI输入视频频率
+	buf[0] = reg[1];
+	msg[0].addr  = addr;
+	msg[0].flags = client->flags;   //flags为0，是写，表示buf是我们要发送的数据
+	msg[0].buf   = buf;             //寄存器地址
+	msg[0].len   = sizeof(buf);     //len为buf的大小，单位是字节
+
+	msg[1].addr  = addr;
+	msg[1].flags = client->flags | IIC_RD;   //flags为1，是读，表示buf是我们要接收的数据
+	msg[1].buf   = buf;          //寄存器的值
+	msg[1].len   = 1;
+	ret = i2c_transfer(client->adapter, msg, 2);
+	if(ret < 0)
+	{
+		printk(KERN_CRIT"read regs from i2c has been failed, ret = %d \r\n", ret);
+		return ret;
+	}
+	pixel_frequency = buf[0];
+
+	//读取OpenLDI视频频率
+	printk(KERN_CRIT"default regadd: 0x%02x---pixel_frequency is  0x%02x hex, DEC :%d", reg[1], pixel_frequency, pixel_frequency);
+	if(bin_arr[1] == 0)
+	{
+		video_frequency  = pixel_frequency / 2;
+		printk(KERN_CRIT"bit 6 is %d, and it detect the mode is Dual-pixel, so the video_frequency of OLDI is %d", bin_arr[1], video_frequency);
+	}
+	else if(bin_arr[1] == 1)
+	{
+		video_frequency  = pixel_frequency;
+		printk(KERN_CRIT"bit 6 is %d, and it detect the mode is Single-pixel, so the video_frequency of OLDI is %d", bin_arr[1], video_frequency);
+	}
+	
+	return 0;
+}
+
 //读寄存器函数：link状态检测连接是否断开
 static int link_detect_8(struct i2c_client *client)
 {
@@ -117,7 +215,7 @@ static int link_detect_8(struct i2c_client *client)
 	u8 addr;
 	addr = paddr&0xff;
 
-	printk(KERN_CRIT"This is %s \n", __func__);
+	printk(KERN_CRIT"\n This is %s ", __func__);
 
 	buf[0] = reg;
 	msg[0].addr  = addr;
@@ -139,46 +237,9 @@ static int link_detect_8(struct i2c_client *client)
 	printk(KERN_CRIT"default regadd: 0x%02x---link_status is 0x%02x", reg, link_status);
 	if(link_status == 5)
 	{
-		printk(KERN_CRIT"link is detected");
+		printk(KERN_CRIT"link is detected,  link_status = 1 \r\n");
 	}
-	else printk(KERN_CRIT"link is not detected, link_status is %d", link_status);
-	return 0;
-}
-
-//读寄存器函数: 当前 OpenLDI 的输入视频频率
-static int rd_ODLI_frequency(struct i2c_client *client)
-{
-	int ret = 0;
-	u8 buf[1];
-	u8 chip_id = 0;
-	//读取OpenLDI输入视频频率的寄存器地址
-	u8 reg = 0x5F;
-	//client->addr = 0x0c;
-	u32 paddr = 0x0c;
-	struct i2c_msg msg[2];
-	u8 addr;
-	addr = paddr&0xff;
-
-	printk(KERN_CRIT"This is %s \n", __func__);
-
-	buf[0] = reg;
-	msg[0].addr  = addr;
-	msg[0].flags = client->flags;   //flags为0，是写，表示buf是我们要发送的数据
-	msg[0].buf   = buf;             //寄存器地址
-	msg[0].len   = sizeof(buf);     //len为buf的大小，单位是字节
-
-	msg[1].addr  = addr;
-	msg[1].flags = client->flags | IIC_RD;   //flags为1，是读，表示buf是我们要接收的数据
-	msg[1].buf   = buf;          //寄存器的值
-	msg[1].len   = 1;
-	ret = i2c_transfer(client->adapter, msg, 2);
-	if(ret < 0)
-	{
-		printk(KERN_CRIT"read regs from i2c has been failed, ret = %d \r\n", ret);
-		return ret;
-	}
-	chip_id = buf[0];
-	printk(KERN_CRIT"default regadd: 0x%02x---chip_id is 0x%02x", reg, chip_id);
+	else printk(KERN_CRIT"link is not detected, link_status is 0");
 	return 0;
 }
 
@@ -197,7 +258,7 @@ static int i2cwrite_regs_8(struct i2c_client *client)
 	//GPIO0-3,GPIO5-8的值设置为输出且拉高，增加使能IIC的寄存器值0xDA
 	u8 val_arr[]  = {0x2A, 0x99, 0x09, 0x99, 0x99, 0xDA}; 
 	addr = paddr&0xff;
-	printk(KERN_CRIT"This is %s \n", __func__);
+	printk(KERN_CRIT"\n This is %s ", __func__);
 
 	for(i = 0; i < 6; i++)
 	{
@@ -237,7 +298,7 @@ static int i2cset_passthrough(struct i2c_client *client)
 	//设置相应寄存器的IIC直通功能使能
 	u8 val = 0xDA;
 	addr = paddr&0xff;
-	printk(KERN_CRIT"This is %s \n", __func__);
+	printk(KERN_CRIT"\n This is %s ", __func__);
     buf[0] = reg;
 	buf[1] = val;
 	msg.addr  = addr;
@@ -269,7 +330,7 @@ static int i2cset_dual_pair(struct i2c_client *client)
 	//设置Dual双通道，双绞线模式相应寄存器的值
 	u8 val = 0x23;
 	addr = paddr&0xff;
-	printk(KERN_CRIT"This is %s \n", __func__);
+	printk(KERN_CRIT"\n This is %s ", __func__);
     buf[0] = reg;
 	buf[1] = val;
 	msg.addr  = addr;
@@ -302,7 +363,7 @@ static int set_gpio_input(struct i2c_client *client)
 	//GPIO0-3,GPIO5-8的值设置为输入
 	u8 val_arr[]  = {0x23, 0x33, 0x03, 0x33, 0x33}; 
 	addr = paddr&0xff;
-	printk(KERN_CRIT"This is %s \n", __func__);
+	printk(KERN_CRIT"\n This is %s ", __func__);
 
 	for(i = 0; i < 5; i++)
 	{
@@ -351,12 +412,18 @@ static int rdreg_probe(struct i2c_client *client, const struct i2c_device_id *id
 {
 	struct device *dev = &client->dev;
 	
-	printk(KERN_CRIT"This is %s \n", __func__);
+	printk(KERN_CRIT"\n This is %s ", __func__);
 	//将GPIO 0-3,GPIO 5-8设置输入模式
-	set_gpio_input(client);
+	//set_gpio_input(client);
+
 	//读取GPIO0-3,GPIO5-8相应寄存器的值并打印
-    i2cread_regs_8(client);
+    //i2cread_regs_8(client);
+
+	//检测link状态
 	link_detect_8(client);
+
+	//读取OLDI的输入视频频率
+	read_video_frequency(client);
 	
     return 0;
 }
